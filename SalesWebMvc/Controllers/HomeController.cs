9458c12f739978b9c61bc318832ff6b7b452e844
pedro-ddias <69222299+pedro-ddias@users.noreply.github.com>
2023-04-13 20:19:52 -0300

    First controller and Razot pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Saller Web MVC App from C# Course";
+            ViewData["email"] = "pedro.hdias@outlook.com";
 
             return View();
         }
